7cfb463d2e89b427455ec256fd6b5c42b22fa14d
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-01-13 21:42:21 +0100

    flats index feature

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+require 'json'
+class FlatsController < ApplicationController
+
+  def index
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(URI.open(url).read)
+
+  end
+
+  def show
+
+  end
+end
